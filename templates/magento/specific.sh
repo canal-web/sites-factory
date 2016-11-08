97dacccd627a2c920a7199d0a6c52e548d3dff04
deb <deborah@canal-web.fr>
2016-11-08 16:05:58 +0100

    change subdomain+domainname to localhost - fixes #3

--- templates/magento/specific.sh
+++ templates/magento/specific.sh
@@ -1,7 +1,7 @@
 
 OLD_HOSTS=`mysql --silent --skip-column-names -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e 'select GROUP_CONCAT(config_id SEPARATOR ",") from core_config_data where value like "%http%" and path like "%web/%" and path like "%secure%" GROUP BY "all"'`
 
-mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e "UPDATE core_config_data SET value='http://${SUB_DOMAIN}.${DOMAIN_NAME}/' WHERE config_id IN ($OLD_HOSTS)"
+mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e "UPDATE core_config_data SET value='http://${LOCAL_HOST}/' WHERE config_id IN ($OLD_HOSTS)"
 
 
 for DIR in session locks cache
