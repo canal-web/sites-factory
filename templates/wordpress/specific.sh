1c3013fcc0b00f2df849df01d21f11733f2acd4a
Fabio <fabiocerqueiracw@gmail.com>
2016-11-23 16:05:23 +0100

    Fix the sql request by passing to one line

--- templates/wordpress/specific.sh
+++ templates/wordpress/specific.sh
@@ -1,14 +1,3 @@
 # Change the URLS
 
-mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e "
-UPDATE wp_options
-SET option_value = replace(option_value, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}')
-WHERE option_name = 'home'
-OR option_name = 'siteurl';
-UPDATE wp_posts
-SET guid = REPLACE (guid, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}');
-UPDATE wp_posts
-SET post_content = REPLACE (post_content, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}');
-UPDATE wp_postmeta
-SET meta_value = REPLACE (meta_value, 'http://${MASTER_SITE_URL}','http://${CHILD_SITE_URL}');
-"
+mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e "UPDATE wp_options SET option_value = replace(option_value,   'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}')  WHERE option_name = 'home' OR option_name = 'siteurl'; UPDATE wp_posts SET guid =  REPLACE (guid, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}'); UPDATE wp_posts SET post_content = REPLACE (post_content, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}'); UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, 'http://${MASTER_SITE_URL}','http://${CHILD_SITE_URL}');"
