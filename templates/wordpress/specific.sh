d408dbe54c6a09dea11d252b004427214a92bd82
Fabio <fabiocerqueiracw@gmail.com>
2016-11-23 15:57:05 +0100

    Change used vars for the file generation

--- templates/wordpress/specific.sh
+++ templates/wordpress/specific.sh
@@ -1,16 +1,14 @@
 # Change the URLS
 
-OLD_DOMAIN=${SITE_NAME}.${DOMAIN_NAME}
-
 mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD $LOCAL_DB_NAME -e "
 UPDATE wp_options
-SET option_value = replace(option_value, 'http://${OLD_DOMAIN}', 'http://${LOCAL_HOST}')
+SET option_value = replace(option_value, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}')
 WHERE option_name = 'home'
 OR option_name = 'siteurl';
 UPDATE wp_posts
-SET guid = REPLACE (guid, 'http://${OLD_DOMAIN}', 'http://${LOCAL_HOST}');
+SET guid = REPLACE (guid, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}');
 UPDATE wp_posts
-SET post_content = REPLACE (post_content, 'http://${OLD_DOMAIN}', 'http://${LOCAL_HOST}');
+SET post_content = REPLACE (post_content, 'http://${MASTER_SITE_URL}', 'http://${CHILD_SITE_URL}');
 UPDATE wp_postmeta
-SET meta_value = REPLACE (meta_value, 'http://${OLD_DOMAIN}','http://${LOCAL_HOST}');
+SET meta_value = REPLACE (meta_value, 'http://${MASTER_SITE_URL}','http://${CHILD_SITE_URL}');
 "
