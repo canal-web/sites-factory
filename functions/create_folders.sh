edf5797f2e3fb12585d409d0c53a47c1dbe9057b
deb <deborah@canal-web.fr>
2017-01-27 18:42:08 +0100

    Adding site-deployment logic

--- functions/create_folders.sh
+++ functions/create_folders.sh
@@ -25,10 +25,22 @@ else
     LOCAL_DIR="${PATH_LOCAL_SITES}${FOLDER_SITE_NAME}.${DOMAIN_NAME}/"
     if [[ ! -d $LOCAL_DIR ]]; then
         sudo mkdir -p "${LOCAL_DIR}httpdocs"
+        sudo mkdir -p "${LOCAL_DIR}production"
         sudo chown -R ${THE_USER}.${LOCAL_GROUP} ${LOCAL_DIR}
         sudo chmod g+w ${LOCAL_DIR}
         sudo mkdir -p "${LOCAL_DIR}logs"
         sudo mkdir -p "${LOCAL_DIR}bin"
         echo "Directories have been created in ${LOCAL_DIR}."
+
+        INSTANCE_MERE_URL="http://${SUB_DOMAIN}.${DOMAIN_NAME}/"
+        git clone git@github.com:canal-web/sites-deployment.git ${LOCAL_DIR}production
+        cat "${LOCAL_DIR}production/settings/default.local_params.sh" | sed \
+        -e "s,_LOCAL_URL,${INSTANCE_MERE_URL}," \
+        -e "s,_LOCAL_ROOTDIR,${LOCAL_DIR}httpdocs," \
+        -e "s,_LOCAL_SQL_HOST,${LOCAL_HOST}," \
+        -e "s,_LOCAL_SQL_USER,${SQL_USER}," \
+        -e "s,_LOCAL_SQL_PASSWORD,${SQL_PASSWD}," \
+        -e "s,_LOCAL_SQL_DATABASE,$(echo "$PROVIDED_DATABASE" | sed 's/[\._-]//g')," \
+         > "${LOCAL_DIR}production/settings/local_params.sh"
     fi
 fi
